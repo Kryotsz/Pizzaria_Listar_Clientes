7bc86c721cc2a4460b0958b8ad92f69db248a6c7
Igor <igorhgassen@gmail.com>
2022-09-15 20:27:47 -0300

    primeiro commit

---
+++ WF_MySqlExecT9/Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace WF_MySqlExecT9
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.ListViewItem listViewItem1 = new System.Windows.Forms.ListViewItem("");
+            this.listViewClientes = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader6 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader7 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader8 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.btnListarClientes = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listViewClientes
+            // 
+            this.listViewClientes.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4,
+            this.columnHeader5,
+            this.columnHeader6,
+            this.columnHeader7,
+            this.columnHeader8});
+            this.listViewClientes.FullRowSelect = true;
+            this.listViewClientes.GridLines = true;
+            this.listViewClientes.HideSelection = false;
+            this.listViewClientes.Items.AddRange(new System.Windows.Forms.ListViewItem[] {
+            listViewItem1});
+            this.listViewClientes.Location = new System.Drawing.Point(12, 84);
+            this.listViewClientes.Name = "listViewClientes";
+            this.listViewClientes.Size = new System.Drawing.Size(651, 270);
+            this.listViewClientes.TabIndex = 0;
+            this.listViewClientes.UseCompatibleStateImageBehavior = false;
+            this.listViewClientes.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Nome Completo";
+            this.columnHeader1.Width = 103;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Data_Nascimento";
+            this.columnHeader2.Width = 104;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Telefone";
+            this.columnHeader3.Width = 88;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "DDD";
+            this.columnHeader4.Width = 40;
+            // 
+            // columnHeader5
+            // 
+            this.columnHeader5.Text = "Endereço";
+            this.columnHeader5.Width = 69;
+            // 
+            // columnHeader6
+            // 
+            this.columnHeader6.Text = "Complemento";
+            this.columnHeader6.Width = 78;
+            // 
+            // columnHeader7
+            // 
+            this.columnHeader7.Text = "CEP";
+            this.columnHeader7.Width = 89;
+            // 
+            // columnHeader8
+            // 
+            this.columnHeader8.Text = "Número";
+            this.columnHeader8.Width = 75;
+            // 
+            // btnListarClientes
+            // 
+            this.btnListarClientes.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnListarClientes.Location = new System.Drawing.Point(12, 13);
+            this.btnListarClientes.Name = "btnListarClientes";
+            this.btnListarClientes.Size = new System.Drawing.Size(651, 65);
+            this.btnListarClientes.TabIndex = 1;
+            this.btnListarClientes.Text = "Listar Clientes";
+            this.btnListarClientes.UseVisualStyleBackColor = true;
+            this.btnListarClientes.Click += new System.EventHandler(this.btnListarClientes_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(676, 366);
+            this.Controls.Add(this.btnListarClientes);
+            this.Controls.Add(this.listViewClientes);
+            this.Name = "Form1";
+            this.Text = "Pizzeria";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listViewClientes;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.Button btnListarClientes;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.ColumnHeader columnHeader5;
+        private System.Windows.Forms.ColumnHeader columnHeader6;
+        private System.Windows.Forms.ColumnHeader columnHeader7;
+        private System.Windows.Forms.ColumnHeader columnHeader8;
+    }
+}
+
---
+++ WF_MySqlExecT9/Form1.cs
@@ -0,0 +1,86 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WF_MySqlExecT9
+{
+    public partial class Form1 : Form
+    {
+        MySqlConnection connection;
+        MySqlCommand command;
+        MySqlDataReader reader;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            connection = new MySqlConnection("Server=localhost;Port=3307;Database=pizzeriadb;Uid=root;Pwd=;SslMode=none");
+        }
+
+        private void btnListarClientes_Click(object sender, EventArgs e)
+        {
+            // cria lista do tipo Cliente
+            List<Cliente> listaClientes = new List<Cliente>();
+            // popular a lista através do select do banco de dados
+            command = new MySqlCommand();
+            // abre a conexão
+            connection.Open();
+            // seta a conexão para o comando
+            command.Connection = connection;
+            command.CommandText = "SELECT nome_completo, data_nascimento, telefone, ddd, endereco, complemento, cep, numero FROM cliente";
+            // executa o comando
+            reader= command.ExecuteReader();
+
+            while (reader.Read())
+            {
+                // cria a variável "cliente" dentro da class "Cliente"
+                Cliente cliente = new Cliente();
+
+                // obtém todos os campos da tabela "cliente" no banco de dados e joga dentro da variável "cliente"
+                cliente.nome_completo = reader["nome_completo"].ToString();
+                cliente.data_nascimento = reader["data_nascimento"].ToString();
+                cliente.telefone = reader["telefone"].ToString();
+                cliente.ddd = reader["ddd"].ToString();
+                cliente.endereco = reader["endereco"].ToString();
+                cliente.complemento = reader["complemento"].ToString();
+                cliente.cep = reader["cep"].ToString();
+                cliente.numero = reader["numero"].ToString();
+
+                ListViewItem Cliente = new ListViewItem();
+
+                // adiciona na listview todos os campos capturados pela variável "cliente"
+                Cliente.Text = cliente.nome_completo;
+                Cliente.SubItems.Add(cliente.data_nascimento);
+                Cliente.SubItems.Add(cliente.telefone);
+                Cliente.SubItems.Add(cliente.ddd);
+                Cliente.SubItems.Add(cliente.endereco);
+                Cliente.SubItems.Add(cliente.complemento);
+                Cliente.SubItems.Add(cliente.cep);
+                Cliente.SubItems.Add(cliente.numero);
+
+                listViewClientes.Items.Add(Cliente);
+            }
+            connection.Close();
+        }
+    }
+
+    // cria class "Cliente"
+    class Cliente
+    {
+        public string nome_completo { get; set; }
+        public string data_nascimento { get; set; }
+        public string telefone { get; set; }
+        public string ddd { get; set; }
+        public string endereco { get; set; }
+        public string complemento { get; set; }
+        public string cep { get; set; }
+        public string numero { get; set; }
+    }
+}
